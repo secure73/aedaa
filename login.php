f1c0007ebc111536166e4421351a8520c000f465
Ali <secure73@gmail.com>
2023-03-22 13:25:20 +0100

    create index register login

--- login.php
+++ login.php
@@ -1,3 +1,26 @@
 <?php 
-echo "login";
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h1>Login</h1>
+<form action="">
+        username:
+            <input  name="username" type="text" >
+            <br>
+            <br>
+            password:
+            <input  name="password" type="password">
+            <br>
+            <input type="submit" value="register">
+    </form>
+    
+</body>
+</html>
