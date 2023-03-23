29bdd3c8c70f172576a9356f1016aadc538c6920
ErikKoop <erikkoop@web.de>
2023-03-23 10:37:49 +0100

    neu

---
+++ app/UserProfile.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+
+class UserProfile {
+        // functions shall camelCase
+        // functions must have clear name
+        // function name shall declear what it doing !!!
+    public function updateList(){
+
+
+    }
+
+}
