4685688d23799aed0e145e64ff086ad6a3656471
summer2111 <124155444+summer2111@users.noreply.github.com>
2023-03-23 10:19:15 +0100

    design auf test seite bearbeitet

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php include('./template/bootstrap.css'); ?>
+<?php include('./template/bootstrap.js.php'); ?>
+<?php include('./template/head.php'); ?>
+<?php include('./template/footer.php'); ?>
+
+
