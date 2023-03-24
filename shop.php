c5991c3345ca124edb253937857b124b437fb999
ErikKoop <erikkoop@web.de>
2023-03-24 10:41:39 +0100

    mmm

--- shop.php
+++ shop.php
@@ -1,9 +1,5 @@
+<?php include('./template/head.php'); ?>
 <?php include('./template/navbar.php'); ?>
-
-<div class="container pt-3">
-    <h3>Shop</h3>
-</div>
-
-
-
+<h1>Version: Shop</h1>
 <?php include('./template/footer.php'); ?>
+
