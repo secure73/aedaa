6c629bd17bde4dfb3d2a12a87b03423ff4095660
TomLittleGit <126460891+TomLittleGit@users.noreply.github.com>
2023-03-24 12:57:55 +0100

    eigenständig gearbeitet

--- app/table/QueryProvider.php
+++ app/table/QueryProvider.php
@@ -3,8 +3,9 @@
  * By Ali Khorsandfard
  */
 declare(strict_types=1);
-
 namespace Table\Database;
+require_once('DatabasePdoConnection.php');
+
 
 class QueryProvider extends DatabasePdoConnection
 {
--- test.php
+++ test.php
@@ -1,17 +1,33 @@
 <?php 
 
-use Table\Database\DatabasePdoConnection;
+use Table\Database\QueryProvider;
 
 include('./template/navbar.php');
-require_once('./app/table/DatabasePdoConnection.php');
 
-$connection = new DatabasePdoConnection();
-$connection->connect();
+require_once('./app/table/QueryProvider.php');
+$qp = new QueryProvider();
+
+//$sqlQuery = 'INSERT INTO users(email, password, isAdmin) VALUES (:email,:password,:isAdmin)';
+//$arrayBind =[':email'=>'myemail@gmail.com',':password'=>'abcdef123',':isAdmin'=>true];
+//$qp->insertQuery($sqlQuery,$arrayBind);
+
+//$updateQuery = 'UPDATE users SET email = :email, password = :password, isAdmin = :isAdmin WHERE id = :id';
+//$arrayUpdateBindValue = [':email'=>'newEmail@gmail.com', ':password'=>'newpass', ':isAdmin'=> false, ':id'=>1];
+//$qp->updateQuery($updateQuery,$arrayUpdateBindValue);
+
+//$deleteQuery = 'DELETE FROM users WHERE id = :id';
+//$arrayBindDelete = [':id'=>3];
+//$qp->deleteQuery($deleteQuery,$arrayBindDelete);
+
+selectQuery = 'SELECT * FROM users WHERE 1';
+$answerSelectQuery = $qp->selectQuery($selectQuery);
+
+
 
 ?>
 <div class="container pt-3">
     <h3>DAA AE Ina Zimmermann</h3>
-    <?=$connection->isConnect()?>
+    <?=var_dump($answerSelectQuery)?>
 </div>
 
 
