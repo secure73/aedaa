256ba8609d8b40b52f163cd739887d065ae6f894
TomLittleGit <126460891+TomLittleGit@users.noreply.github.com>
2023-03-24 12:07:00 +0100

    test

--- test.php
+++ test.php
@@ -1,7 +1,14 @@
-<?php include('./template/navbar.php'); ?>
+<?php include('./template/navbar.php');
 
+require_once('./app/table/DatabasePdoConnection.php');
+
+$connection = new DatabasePdoConnection();
+$connection->connect();
+
+?>
 <div class="container pt-3">
     <h3>DAA AE Ina Zimmermann</h3>
+    <?=$connection->isConnect()?>
 </div>
 
 
