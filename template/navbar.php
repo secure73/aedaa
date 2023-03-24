00025e2c3cd9c46b0f28ed95cc2ecf084463ee33
ayildirim34 <ayildirim3457@protonmail.com>
2023-03-24 09:15:51 +0100

    ahmet

--- template/navbar.php
+++ template/navbar.php
@@ -18,6 +18,9 @@
         <li class="nav-item">
           <a class="nav-link" href="test.php">test</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="admin.php">admin</a>
+        </li>
       </ul>
     </div>
   </div>
