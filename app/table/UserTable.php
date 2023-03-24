7f5e6c00982b16506688e02d7e1323582e80fb57
KarstenKutschke <123940947+KarstenKutschke@users.noreply.github.com>
2023-03-24 12:50:47 +0100

    test

---
+++ app/table/UserTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Table;
+
+class UserTable
+{
+
+   public int $id;
+   public string $email;
+   public string $password;
+   // when we have boolean var, please use "is" before variable
+   public bool $isAdmin;
+
+   //php class constructor
+   
+   public function insert(string $email, string $password): int
+   {
+      return 1;
+   }
+   public function delete(string $email, string $password): bool
+   {
+      return false;
+   }
+   public function updatePassword(string $password): bool
+   {
+      return false;
+   }
+
+   public function updateAdmin(bool $isAdmin): bool
+   {
+      return false;
+   }
+}
