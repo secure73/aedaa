d2b1f95f710a14e756b5f3ea209d2c0fb3bd2a97
DrKnubo <s.schumacher-konstanz@gmx.de>
2023-03-24 08:42:03 +0100

    fertig

--- template/navbar.php
+++ template/navbar.php
@@ -15,6 +15,9 @@
         <li class="nav-item">
           <a class="nav-link" href="login.php">login</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="test.php">Schumi Test</a>
+        </li>
       </ul>
     </div>
   </div>
--- test.php
+++ test.php
@@ -1,1 +1,14 @@
 <?php echo"<h1>Schumi version</1>"; ?>
+
+
+<?php include('./template/navbar.php'); ?>
+
+
+<div class="container pt-3">
+
+<h1>Schumi version</1>
+</div>
+
+
+
+<?php include('./template/footer.php'); ?>
