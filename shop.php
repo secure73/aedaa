87f298690885fafeb1bab9d656e0488646c5291e
summer2111 <124155444+summer2111@users.noreply.github.com>
2023-03-24 09:19:27 +0100

    Shop Seite

---
+++ shop.php
@@ -0,0 +1,9 @@
+<?php include('./template/head.php'); ?>
+
+<?php include('./template/navbar.php'); ?>
+<br>
+<br>
+<br>
+<h1>Welcome Admin</h1>
+
+<?php include('./template/footer.php'); ?>
--- template/navbar.php
+++ template/navbar.php
@@ -21,6 +21,9 @@
         <li class="nav-item">
           <a class="nav-link" href="admin.php">Admin</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="shop.php">Shop</a>
+        </li>
       </ul>
     </div>
   </div>
