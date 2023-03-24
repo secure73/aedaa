d5d07af44bad032cb9d0480f7bc143ba8cb10b15
Manolo-AE <124036561+Manolo-AE@users.noreply.github.com>
2023-03-24 10:05:59 +0100

    merge

--- app/model/User.php
+++ app/model/User.php
@@ -1,70 +1,5 @@
 <?php
 namespace App;
-<<<<<<< HEAD
-
-class User {
-    public int $id;
-    public string $email;
-    public string $password;
-    // when we habe boolean type variable, please use "is" before the variableName 
-    public bool $isAdmin;
-
-    //php class constructor
-    public function __construct()
-    {
-        return;
-    }
-
-    /**
-     * @return int in case of success return last inserted id from creatd User
-     * @return false in case of unseccessfull creation return false
-     */
-    public function createUser(string $email , string $password):int|false
-    {
-        
-        return false;
-    }
-
-
-    public function makeUserAdmin(string $email):bool
-    {
-        return false;
-    }
-
-    public function removeAdmin(string $email):bool
-    {
-        return false;
-    }
-    /**
-     * @return User if user existed
-     * @return false if user cannot be found, or email dosent existed in out Database
-     */
-    public function findUserByEmail(string $email):User|false
-    {
-        return false;
-    }
-
-    
-    public function login(string $email , string $password):bool
-    {
-        return false;
-    }
-
-    public function logout():void
-    {
-
-    }
-
-    /**
-     *@return array<User> can ne array of null
-     */	
-    public function getUsers():array
-    {
-        $arrayUsers =[];
-        //code to get all users from database
-        return $arrayUsers;
-    }
-=======
 class User{
    public int $id;
    public string $email;
@@ -115,7 +50,6 @@ $arrayUsers = [];
 //code to get all users from database
 return $arrayUsers;
 }
->>>>>>> origin/Zimmermann
 
 
 
