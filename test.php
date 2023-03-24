2d6a752dbd3ea5d21c781d456b2d9c3d7439d563
summer2111 <124155444+summer2111@users.noreply.github.com>
2023-03-24 08:48:35 +0100

    Design bearbeitet

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 
 <?php include('./template/head.php'); ?>
-<?php include('./template/footer.php'); ?>
+
 <?php include('./template/navbar.php'); ?>
 
 <h1>DAA AE Nadine Stamm</h1>
+<?php include('./template/footer.php'); ?>
 
