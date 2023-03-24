5dc8b6b39849b9f9ceda373439fca3ecd510945b
ErikKoop <erikkoop@web.de>
2023-03-24 13:04:55 +0100

    sss

--- test.php
+++ test.php
@@ -7,17 +7,29 @@ include('./template/navbar.php');
 require_once('./app/table/QueryProvider.php');
 $qp = new QueryProvider();
 
-$sqlQuery = 'INSERT INTO users (email ,password, isAdmin) VALUES (:email,:password,:isAdmin)';
-$arrayBind = [':email'=>'myemail@gmail.com',':password'=>'sajhdjadd',':isAdmin'=>true];
+//$sqlQuery = 'INSERT INTO users (email ,password, isAdmin) VALUES (:email,:password,:isAdmin)';
+//$arrayBind = [':email'=>'myemail@gmail.com',':password'=>'sajhdjadd',':isAdmin'=>true];
+//$userId = $qp->insertQuery($sqlQuery,$arrayBind);
 
-$userId = $qp->insertQuery($sqlQuery,$arrayBind);
+//$updateQuery = 'UPDATE users SET email= :email ,password = :password , isAdmin = :isAdmin WHERE id = :id';
+//$arrayUpdateBindValue = ['email'=> 'newEmail@gmail.com', ':password' => 'ewpass' , 'isAdmin' => false, ':id' => 1 ];
+//$qp->updateQuery($updateQuery, $arrayUpdateBindValue);
+
+
+$deleteQuery = 'DELETE FROM users WHERE id = :id';
+$arrayBindDelete = [':id'  => 3];
+$qp->deleteQuery($deleteQuery)
+
+
+$selectQuery = 'SELECT * FROM users WHERE 1';
+$answerSelectQuery = $qp->selectQuery($selectQuery);
 
 
 ?>
 
 <div class="container pt-3">
     <h3>DAA AE Ina Zimmermann</h3>
-    <?=var_dump($qp->getError())?>
+    <?=var_dump($answerSelectQuery)?>
 </div>
 
 
