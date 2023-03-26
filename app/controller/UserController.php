cd329f30b0b4e74a3c6fc40b54c419e174935f0f
ErikKoop <erikkoop@web.de>
2023-03-26 22:33:37 +0200

    a

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-create some functions
-=======
-create some functios
->>>>>>> origin/dev
-=======
 <?php
 namespace App\Controller;
 class UserController {
 
    
 }
->>>>>>> dev
+
