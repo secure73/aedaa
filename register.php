bd261196816070db9a3d689be3b8476d6af9a679
Manolo-AE <124036561+Manolo-AE@users.noreply.github.com>
2023-03-27 11:55:56 +0200

    update register.php

--- register.php
+++ register.php
@@ -1,26 +1,23 @@
-<?php include('./template/navbar.php'); 
-include('./template/navbar.php');
+<?php
+
+use App\Controller\RegisterController;
+include('./template/navbar.php'); 
 require_once('./app/controller/RegisterController.php');
-
-if (isset($_POST ))
-{
-  var_dump($_POST);
-}
-
-
+ $register = new RegisterController();
+ $register->register();
 ?>
 
 <h1>Register</h1>
 <div class="card flex m-5 p-5 bg-secondary">
-<form method="post" target="_self" >
+<form  method="post" target="_self">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input  name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input name="pasword" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input  name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" minlength="4">
   </div>
 
   <br>
