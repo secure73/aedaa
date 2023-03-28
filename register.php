6417473ddbe36cb1a52d109dba13698b66cf656a
inaZimmermann <inazimmermann1996qgmail.com>
2023-03-28 07:59:29 +0200

    some changes

--- register.php
+++ register.php
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<?php include('./template/navbar.php');
-require_once('./app/controller/RegisterController.php');
-
-
-=======
 <?php
 
 use App\Controller\RegisterController;
@@ -11,31 +5,19 @@ include('./template/navbar.php');
 require_once('./app/controller/RegisterController.php');
  $register = new RegisterController();
  $register->register();
->>>>>>> origin/dev
 ?>
 
 <h1>Register</h1>
 <div class="card flex m-5 p-5 bg-secondary">
-<<<<<<< HEAD
-<form method="post" target="_self">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input name = "email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-=======
 <form  method="post" target="_self">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input  name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
->>>>>>> origin/dev
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-<<<<<<< HEAD
-    <input name = "password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-=======
     <input  name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" minlength="4">
->>>>>>> origin/dev
   </div>
 
   <br>
